7570ce047af64589354088ca1d037aca7613f9e9
CitlaCH2 <hdz.citla.2@gmail.com>
2025-07-31 17:29:22 -0600

    Se agregan precios

--- js/main.js
+++ js/main.js
@@ -2,13 +2,23 @@ const btnAgregar = document.getElementById("btnAgregar");
 const btnClear = document.getElementById("btnClear");
 const txtName = document.getElementById("Name");
 const txtNumber = document.getElementById("Number");
+
 const alertValidaciones = document.getElementById("alertValidaciones");
 const alertValidacionesTexto = document.getElementById("alertValidacionesTexto");
 const tablaListaCompras =document.getElementById("tablaListaCompras");
 const cuerpoTabla = tablaListaCompras.getElementsByTagName("tbody").item(0);
 
+const contadorProductos = document.getElementById("contadorProductos");
+const productosTotal = document.getElementById("productosTotal");
+const precioTotal = document.getElementById("precioTotal");
 
-let cont =0; //Cuenta los productos agregados
+let cont = 0;
+let totalEnProductos = 0;
+let costoTotal = 0;
+
+
+
+//let cont =0; //Cuenta los productos agregados
 
 function validarCantidad(){
     if (txtNumber.value.length==0){
@@ -70,7 +80,16 @@ let row = `<tr>
 `;
 
 cuerpoTabla.insertAdjacentHTML("beforeend", row);
-    txtName.value ="";
+contadorProductos.innerText = cont;
+totalEnProductos += Number(txtNumber.value);
+productosTotal.innerText = totalEnProductos;
+costoTotal +=precio * Number(txtNumber.value);
+//costoTotal.toFixed(2); //forma fácil 
+precioTotal.innerText = new Intl.NumberFormat("es-MX", 
+                    { style: "currency", currency: "MXN" }).format(costoTotal);
+
+
+txtName.value ="";
     txtNumber.value ="";
     txtName.focus();
 
